d7f4a1f260ab286a7baa1eaf286461c9435ad40b
Ryan <ryanfrost2015@gmail.com>
2019-10-16 15:40:10 +0100

    Verion one and two

---
+++ my_functions.rb
@@ -0,0 +1,11 @@
+def add_array_lengths(array1, array2)
+  return array1.length + array2.length
+end
+
+def sum_array(numbers)
+  total = 0
+  for number in numbers
+    total = total + number
+  end
+  return total
+end
